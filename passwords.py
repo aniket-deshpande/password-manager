be9e37a41eb7f9924c040fbb3665d5c13ef4fd85
aniket-deshpande <69606790+aniket-deshpande@users.noreply.github.com>
2020-11-11 09:13:15 -0500

    Add files via upload

---
+++ passwords.py
@@ -0,0 +1,58 @@
+from os import system, name
+import time
+
+def clear():  
+    if name == 'nt': 
+        _ = system('cls')
+
+clear()
+
+def run():
+    clear() 
+    
+    print("*"*10)
+    print("\nCOMMANDS\n")
+    print("ls - list current passwords\na - add new password\nq - quit\n")
+    print("*"*10)
+   
+    cmd = ""
+
+    while cmd != "q": 
+        cmd = input("\nWhat do you want to do?: ")
+
+        if cmd == "ls":
+            with open("list.txt", "r") as fl:
+                content = fl.read() 
+                print("\n" + content)
+            print()
+        elif cmd == "a":
+            with open("list.txt", "a") as f:
+                print()
+                newsite = input("Enter a new website: ")
+                newusr = input("Enter the new username: ")
+                newps = input("Enter the new password: ")
+                f.write(f"\n{newsite}\n{newusr}\n{newps}")
+            print()
+        if cmd not in "qals":
+            print("Unknown command entered. Quitting program in 5 seconds")
+            time.sleep(5)
+            quit()
+    clear()
+    quit()
+
+
+def unlock():
+    i = 0
+    clear()
+    master = input("Unlock the manager: ")
+    if master == "0321":
+        run()
+    else:
+        while i < 5:
+            print("This is the wrong pin")
+            unlock()
+        print("Too many incorrect entries. Closing program now...")
+        time.sleep(2)
+        quit()
+        
+unlock()
